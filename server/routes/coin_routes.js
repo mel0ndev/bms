469c0d1543ac8abbde768313eb46894caeba2207
kevin <arkitectmc@gmail,com>
2022-06-15 09:56:35 -0400

    Revert"basic data gathering no websocket"
    This reverts commit 82c864f588cf93297e0bab7c4883ee8806498ab6.

--- server/routes/coin_routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express'); 
-const router = express.Router(); 
-const coins_controller = require('../controllers/coins_controller.js'); 
-
-module.exports = router; 
-
-router.get('/api', coins_controller.list_coins)
